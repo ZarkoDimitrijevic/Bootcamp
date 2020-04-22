4e3fdc7c033238b7f5ea6065ced732757db1f77c
ZarkoDimitrijevic <dimitrijevic.zarko@gmail.com>
2020-04-23 00:04:45 +0200

    2342020

---
+++ Vezbanja/Vezbanje januar 2020/vezbanje.php
@@ -0,0 +1,18 @@
+﻿<?php
+$techstack = 'Ajax, PHP, Javascript, CSS, HTML';
+if(strpos($techstack,","))
+{
+    $check=explode(",", $techstack);
+    var_dump($check);
+    foreach($check as $techstack)
+    {
+        $techstack=str_replace(' ', '', $techstack);
+        //$query->where('candidates.techstack','LIKE', "%{$techstack}%");
+        var_dump($techstack);
+
+        echo "<br>";
+    }
+};
+
+
+?>
